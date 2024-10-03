e106318751a4e6f5c1cd8d2b55b86a10bec4d36a
0chir03 <oshirvandanov@gmail.com>
2024-10-03 16:59:20 +0700

    маршрутизация

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "Hello World!";
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+if ($requestUri === '/login') {
+    if ($requestMethod === 'GET') {
+        require_once './get_login.php';
+    } elseif ($requestMethod === 'POST') {
+        require_once './handle_login.php';
+    } else {
+        echo "$requestMethod не поддерживается адресом $requestUri";
+    }
+} elseif ($requestUri === '/registrate') {
+    if ($requestMethod === 'GET') {
+        require_once './get_registration.php';
+    } elseif ($requestMethod === 'POST') {
+        require_once './handle_registration.php';
+    }  else {
+        echo "$requestMethod не поддерживается адресом $requestUri";
+    }
+} elseif ($requestUri === '/catalog') {
+    if ($requestMethod === 'GET') {
+        require_once './catalog.php';
+    }
+}
+else {
+    require_once './404.php';
+}
