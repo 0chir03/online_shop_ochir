9949ca3acda2916778e7e5fb152e4c4ca0202632
0chir03 <oshirvandanov@gmail.com>
2024-10-06 21:12:38 +0700

    добавление маршрута для корзины

--- src/public/index.php
+++ src/public/index.php
@@ -22,8 +22,23 @@ if ($requestUri === '/login') {
 } elseif ($requestUri === '/catalog') {
     if ($requestMethod === 'GET') {
         require_once './catalog.php';
+    } else {
+        echo "$requestMethod не поддерживается адресом $requestUri";
     }
-}
-else {
+} elseif ($requestUri === '/add-product'){
+    if ($requestMethod === 'GET') {
+        require_once './get_add_product.php';
+    } elseif ($requestMethod === "POST") {
+        require_once './handle_add_product.php';
+    } else {
+        echo "$requestMethod не поддерживается адресом $requestUri";
+    }
+} elseif ($requestUri === '/card') {
+    if ($requestMethod === 'GET') {
+        require_once './get_card.php';
+    } else {
+        echo "$requestMethod не поддерживается адресом $requestUri";
+    }
+} else {
     require_once './404.php';
 }
