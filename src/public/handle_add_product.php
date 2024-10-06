9e01d10d80d7cf5958112a2d41596357d1d94d87
0chir03 <oshirvandanov@gmail.com>
2024-10-06 23:57:49 +0700

    редактирование кода

--- src/public/handle_add_product.php
+++ src/public/handle_add_product.php
@@ -3,24 +3,29 @@
 session_start();
 $errors = [];
 
-if (isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ./login');
+} else {
     $user_id = $_SESSION['user_id'];
-    if (!empty($_POST['product_id'])) {
-            $product_id = $_POST['product_id'];
-                 if (!empty($_POST['amount'])) {
-                    $amount = $_POST['amount'];
-                    $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
-                    $stmt = $pdo->prepare("INSERT INTO user_products (user_id, product_id, amount) VALUES (:user_id, :product_id, :amount)");
-                    $stmt->execute(['user_id' => $user_id, 'product_id' => $product_id, 'amount' => $amount]);
-                    header("Location: ./card");
-                } else {
-                     $errors['amount'] = 'Укажите необходимое количество продукта';
-                  }
-            } else {
-                $errors['product_id'] = 'Выберите продукт';
-         }
-    } else {
-        header('Location: ./login');
+}
+
+if (empty($_POST['product_id'])) {
+    $errors['product_id'] = 'Выберите продукт';
+} else {
+    $product_id = $_POST['product_id'];
+}
+
+if (empty($_POST['amount'])) {
+    $errors['amount'] = 'Укажите необходимое количество продукта';
+} else {
+    $amount = $_POST['amount'];
+}
+
+if (empty($errors)) {
+    $pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+    $stmt = $pdo->prepare("INSERT INTO user_products (user_id, product_id, amount) VALUES (:user_id, :product_id, :amount)");
+    $stmt->execute(['user_id' => $user_id, 'product_id' => $product_id, 'amount' => $amount]);
+    header("Location: ./card");
 }
 
 require_once './get_add_product.php';
