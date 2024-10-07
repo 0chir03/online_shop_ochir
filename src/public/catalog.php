f010407863e96e1c6e399746ef65e3a606a0fc55
0chir03 <oshirvandanov@gmail.com>
2024-10-07 16:45:34 +0700

    добавление товара в корзину через кнопку

--- src/public/catalog.php
+++ src/public/catalog.php
@@ -15,17 +15,35 @@ if (!isset($_SESSION['user_id'])) {
     <h3>Catalog</h3>
     <div class="card-deck">
         <?php foreach ($products as $product): ?>
-        <div class="card text-center">
-            <a href="#">
-                <img class="card-img-top" src="<?php echo $product['image']?>" alt="Card image">
-                <div class="card-body">
-                    <p class="card-text text-muted"><?php echo $product['name']; ?></p>
-                    <div class="card-footer">
-                        <?php echo $product['price']; ?>
-                    </div>
+            <form action="/add-product" method="POST">
+                <div class="card text-center">
+                    <a href="#">
+                        <img class="card-img-top" src="<?php echo $product['image']?>" alt="Card image">
+                        <div class="card-body">
+                            <p class="card-text text-muted"><?php echo $product['name']; ?></p>
+                            <div class="card-footer">
+                                <?php echo $product['price']; ?>
+                            </div>
+                                </div>
+                                <label style="color: red;">
+                                    <?php if (!empty($errors['product_id'])) {
+                                        print_r($errors['product_id']);
+                                    }?>
+                                </label>
+                                <input type="text" placeholder="Enter product-id" hidden name="product_id" value="<?php echo $product['id']; ?>" id="product_id" required>
+
+                                <label for="amount"><b>Amount</b></label>
+                                <label style="color: red">
+                                    <?php if (!empty($errors['amount'])) {
+                                        print_r($errors['amount']);
+                                    }?>
+                                </label>
+                                <input type="text" placeholder="Enter amount" name="amount" id="amount" required>
+
+                        <button type="submit" class="registerbtn">Добавить</button>
+                    </a>
                 </div>
-            </a>
-        </div>
+            </form>
         <?php endforeach; ?>
     </div>
 </div>
