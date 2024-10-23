95191c98a9c58e798fac5281524b4725d28a2141
0chir03 <oshirvandanov@gmail.com>
2024-10-23 13:07:40 +0700

    подключение к БД через модельку

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -9,7 +9,7 @@ use Model\OrderProduct;
 use Model\Product;
 use Model\UserProduct;
 
-class OrderService extends Model
+class OrderService
 {
     private Order $order;
     private Product $product;
@@ -22,45 +22,44 @@ class OrderService extends Model
         $this->product = new Product();
         $this->userProduct = new UserProduct();
         $this->orderProduct = new OrderProduct();
-        parent::__construct();
     }
 
     public function create(CreateOrderDTO $orderDTO)
     {
-        $this->pdo->beginTransaction();
-
-       try {
-
            $array = $this->product->getByUserId($orderDTO->getUserId());
            $sum = 0;
            foreach ($array as $key) {
                $sum = $sum + $key->getPrice() * $key->getAmount();
            }
-
-           $order_id = $this->order->create($orderDTO->getContactName(),
-               $orderDTO->getContactPhone(),
-               $orderDTO->getAddress(),
-               $orderDTO->getUserId(),
-               $sum
-           );
-
            $data = $this->userProduct->getByUserId($orderDTO->getUserId());
 
-           foreach ($data as $item) {
-               $product = $item->getProduct();
-               $product_id = $product->getId();
-               $price = $product->getPrice();
-               $amount = $item->getAmount();
-               $total_price = $price * $amount;
+           $pdo = Model::getPDO();
+           $pdo->beginTransaction();
 
-               $this->orderProduct->create($order_id, $product_id, $amount, $total_price);
-           }
-           $this->userProduct->deleteByUserId($orderDTO->getUserId());
-       } catch (\PDOException $exception) {
+           try {
+                $order_id = $this->order->create($orderDTO->getContactName(),
+                                                 $orderDTO->getContactPhone(),
+                                                 $orderDTO->getAddress(),
+                                                 $orderDTO->getUserId(),
+                                                 $sum
+                                                );
 
-           $this->pdo->rollBack();
+               foreach ($data as $item) {
+                   $product = $item->getProduct();
+                   $product_id = $product->getId();
+                   $price = $product->getPrice();
+                   $amount = $item->getAmount();
+                   $total_price = $price * $amount;
+
+                   $this->orderProduct->create($order_id, $product_id, $amount, $total_price);
+               }
+               $this->userProduct->deleteByUserId($orderDTO->getUserId());
+
+           } catch (\PDOException $exception) {
+
+           $pdo->rollBack();
            throw $exception;
        }
-       $this->pdo->commit();
+       $pdo->commit();
     }
 }
